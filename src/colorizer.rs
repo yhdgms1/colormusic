a108f3fc55cfe5f89a087396641612390ba5d3a0
Artemiy Schukin <shuckinart@yandex.ru>
2024-12-25 16:32:09 +0300

    okay

--- src/colorizer.rs
+++ src/colorizer.rs
@@ -5,10 +5,13 @@ pub fn spectrum_to_color(spectrum: Spectrum) -> (f32, f32, f32) {
         return (0f32, 0f32, 0f32);
     }
 
+    let lightness = (spectrum.bass + spectrum.midrange + spectrum.presence) / 300.0;
     let hue = 360.0 * spectrum.sub_bass.min(500.0).hypot(spectrum.brilliance.min(2000.0)) / 3000.0;
 
     dbg!(hue);
 
+    
+
     // shit
-    return (0.7, 0.3, hue);
+    return (lightness.clamp(0.0, 0.7), 0.3, hue);
 }
